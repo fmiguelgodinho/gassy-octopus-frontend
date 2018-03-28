b98197887f81dbaef1d2f8c71e22572cca1b0115
Francisco Godinho <f.miguelgodinho@gmail.com>
2018-03-28 22:58:56 +0100

    script runs frontend and orderer successfully

--- orderer-bftfrontend.sh
+++ orderer-bftfrontend.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+set -ev
+
 # run java component: bft-smart frontend
 cd ${GOPATH}/src/github.com/hyperledger/fabric/hyperledger-bftsmart
-/bin/bash -c -D "./startFrontend.sh ${BFTSMART_FRONTEND_ID} ${BFTSMART_CONNECTION_POOL_SIZE} ${BFTSMART_RECVPORT}"
+./startFrontend.sh $BFTSMART_FRONTEND_ID $BFTSMART_CONNECTION_POOL_SIZE $BFTSMART_RECVPORT & disown
 
 # wait a few seconds to allow java component to boot up
-sleep 15
+sleep 40
 
 # run go component: orderer binary
 cd /opt/gopath/src/github.com/hyperledger/fabric
